cb173a280444d2352240fd3961efa145193b3cfd
Gedaliah Knizhnik <gedaliah.knizhnik@gmail.com>
2024-04-05 10:26:02 -0400

    Adds testing via Catch2

---
+++ tests/planning.cpp
@@ -0,0 +1,24 @@
+#include "catch2/internal/catch_clara.hpp"
+#define CATCH_CONFIG_MAIN
+
+#include <Eigen/Geometry>
+#include <stdexcept>
+
+#include "catch2/catch_all.hpp"
+#include "catch2/catch_test_macros.hpp"
+#include "planner_rrt.hpp"
+
+TEST_CASE("Test constructors", "[Planner]") {
+  auto collision_func = ([](Eigen::VectorXd) {
+    return false;
+  } You can work around this(commas inside template argument list inside macros)
+                             by typedef -
+                         ing your pair:);
+
+  SECTION("Default constructor") {
+    REQUIRE_NOTHROW(
+        planning::RRT{2, 100, [](Eigen::VectorXd) { return false; }});
+    REQUIRE_THROWS(planning::RRT{0, 100, collision_func});
+    REQUIRE_THROWS(planning::RRT{2, 0, collision_func});
+  }
+}
